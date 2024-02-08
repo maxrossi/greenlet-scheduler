d4f246f009cde67f2bfa7af4127059df5fc8be2a
James Hawk <gilbert@ccpgames.com>
2024-02-08 09:42:15 +0000

    Add stubs for all required C-API exposed functions

--- src/SchedulerModule.cpp
+++ src/SchedulerModule.cpp
@@ -18,13 +18,105 @@ C Interface
 extern "C"
 {
 	// Tasklet functions
+	static PyTasklet_Check_RETURN PyTasklet_Check PyTasklet_Check_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyTasklet_Check Not yet implemented" ); //TODO
+		return 0;
+	}
 
-	// Channel functions
+	static PyTasklet_Setup_RETURN PyTasklet_Setup PyTasklet_Setup_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyTasklet_Setup Not yet implemented" ); //TODO
+		return 0;
+	}
+
+    static PyTasklet_Insert_RETURN PyTasklet_Insert PyTasklet_Insert_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyTasklet_Insert Not yet implemented" ); //TODO
+		return 0;
+	}
+
+    static PyTasklet_GetBlockTrap_RETURN PyTasklet_GetBlockTrap PyTasklet_GetBlockTrap_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyTasklet_GetBlockTrap Not yet implemented" ); //TODO
+		return 0;
+	}
+
+    static PyTasklet_SetBlockTrap_RETURN PyTasklet_SetBlockTrap PyTasklet_SetBlockTrap_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyTasklet_SetBlockTrap Not yet implemented" ); //TODO
+	}
+
+    static PyTasklet_IsMain_RETURN PyTasklet_IsMain PyTasklet_IsMain_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyTasklet_IsMain Not yet implemented" ); //TODO
+		return 0;
+	}
+        
+    // Channel functions
+	static PyChannel_New_RETURN PyChannel_New PyChannel_New_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyChannel_New Not yet implemented" ); //TODO
+		return NULL;
+	}
+
+    static PyChannel_Send_RETURN PyChannel_Send PyChannel_Send_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyChannel_Send Not yet implemented" ); //TODO
+		return 0;
+	}
+
+    static PyChannel_Receive_RETURN PyChannel_Receive PyChannel_Receive_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyChannel_Receive Not yet implemented" ); //TODO
+		return NULL;
+	}
+
+    static PyChannel_SendException_RETURN PyChannel_SendException PyChannel_SendException_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyChannel_SendException Not yet implemented" ); //TODO
+		return 0;
+	}
+
+    static PyChannel_GetQueue_RETURN PyChannel_GetQueue PyChannel_GetQueue_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyChannel_GetQueue Not yet implemented" ); //TODO
+		return NULL;
+	}
+
+    static PyChannel_SetPreference_RETURN PyChannel_SetPreference PyChannel_SetPreference_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyChannel_SetPreference Not yet implemented" ); //TODO
+	}
+
+    static PyChannel_GetBalance_RETURN PyChannel_GetBalance PyChannel_GetBalance_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyChannel_GetBalance Not yet implemented" ); //TODO
+		return 0;
+	}
 	
 	// Scheduler functions
-	static PyScheduler_GetCurrent_RETURN PyScheduler_GetCurrent PyScheduler_GetCurrent_PROTO
+	static PyScheduler_Schedule_RETURN PyScheduler_Schedule PyScheduler_Schedule_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyScheduler_Schedule Not yet implemented" ); //TODO
+		return NULL;
+	}
+
+    static PyScheduler_GetRunCount_RETURN PyScheduler_GetRunCount PyScheduler_GetRunCount_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyScheduler_GetRunCount Not yet implemented" ); //TODO
+		return 0;
+	}
+    
+    static PyScheduler_GetCurrent_RETURN PyScheduler_GetCurrent PyScheduler_GetCurrent_PROTO
 	{
 		return reinterpret_cast<PyObject*>(g_scheduler->get_current());
+	}
+
+	static PyScheduler_RunWatchdogEx_RETURN PyScheduler_RunWatchdogEx PyScheduler_RunWatchdogEx_PROTO
+	{
+		PyErr_SetString( PyExc_RuntimeError, "PyScheduler_RunWatchdogEx Not yet implemented" ); //TODO
+		return NULL;
 	}
 
 }	// extern C
@@ -118,7 +210,28 @@ PyInit__scheduler(void)
 
 	//C_API
 	/* Initialize the C API pointer array */
+    // Tasklet Functions
+	PyScheduler_API[PyTasklet_Check_NUM] = (void*)PyTasklet_Check;
+	PyScheduler_API[PyTasklet_Setup_NUM] = (void*)PyTasklet_Setup;
+	PyScheduler_API[PyTasklet_Insert_NUM] = (void*)PyTasklet_Insert;
+	PyScheduler_API[PyTasklet_GetBlockTrap_NUM] = (void*)PyTasklet_GetBlockTrap;
+	PyScheduler_API[PyTasklet_SetBlockTrap_NUM] = (void*)PyTasklet_SetBlockTrap;
+	PyScheduler_API[PyTasklet_IsMain_NUM] = (void*)PyTasklet_IsMain;
+
+    // Channel Functions
+	PyScheduler_API[PyChannel_New_NUM] = (void*)PyChannel_New;
+	PyScheduler_API[PyChannel_Send_NUM] = (void*)PyChannel_Send;
+	PyScheduler_API[PyChannel_Receive_NUM] = (void*)PyChannel_Receive;
+	PyScheduler_API[PyChannel_SendException_NUM] = (void*)PyChannel_SendException;
+	PyScheduler_API[PyChannel_GetQueue_NUM] = (void*)PyChannel_GetQueue;
+	PyScheduler_API[PyChannel_SetPreference_NUM] = (void*)PyChannel_SetPreference;
+	PyScheduler_API[PyChannel_GetBalance_NUM] = (void*)PyChannel_GetBalance;
+
+    // Scheduler Functions
+	PyScheduler_API[PyScheduler_Schedule_NUM] = (void*)PyScheduler_Schedule;
+	PyScheduler_API[PyScheduler_GetRunCount_NUM] = (void*)PyScheduler_GetRunCount;
 	PyScheduler_API[PyScheduler_GetCurrent_NUM] = (void*)PyScheduler_GetCurrent;
+	PyScheduler_API[PyScheduler_RunWatchdogEx_NUM] = (void*)PyScheduler_RunWatchdogEx;
 
 	/* Create a Capsule containing the API pointer array's address */
 	c_api_object = PyCapsule_New( (void*)PyScheduler_API, "scheduler._C_API", NULL );
--- src/SchedulerModule.h
+++ src/SchedulerModule.h
@@ -8,30 +8,181 @@ extern "C"
 /* Header file for schedulermodule */
 
 /* C API functions */
-#define PyScheduler_GetCurrent_NUM 0
+class PyTasklet;
+class PyChannel;
+
+// Tasklet Functions
+#define PyTasklet_Check_NUM 0
+#define PyTasklet_Check_RETURN int
+#define PyTasklet_Check_PROTO ( PyTasklet* )
+
+#define PyTasklet_Setup_NUM 1
+#define PyTasklet_Setup_RETURN int
+#define PyTasklet_Setup_PROTO ( PyTasklet*, PyObject* args, PyObject* kwds )
+
+#define PyTasklet_Insert_NUM 2
+#define PyTasklet_Insert_RETURN int
+#define PyTasklet_Insert_PROTO ( PyTasklet* )
+
+#define PyTasklet_GetBlockTrap_NUM 3
+#define PyTasklet_GetBlockTrap_RETURN int
+#define PyTasklet_GetBlockTrap_PROTO ( PyTasklet* )
+
+#define PyTasklet_SetBlockTrap_NUM 4
+#define PyTasklet_SetBlockTrap_RETURN void
+#define PyTasklet_SetBlockTrap_PROTO ( PyTasklet*, int )
+
+#define PyTasklet_IsMain_NUM 5
+#define PyTasklet_IsMain_RETURN int
+#define PyTasklet_IsMain_PROTO ( PyTasklet* )
+
+// Channel Functions
+#define PyChannel_New_NUM 6
+#define PyChannel_New_RETURN PyChannel*
+#define PyChannel_New_PROTO ( PyTypeObject* )
+
+#define PyChannel_Send_NUM 7
+#define PyChannel_Send_RETURN int
+#define PyChannel_Send_PROTO ( PyChannel*, PyObject* )
+
+#define PyChannel_Receive_NUM 8
+#define PyChannel_Receive_RETURN PyObject*
+#define PyChannel_Receive_PROTO ( PyChannel* )
+
+#define PyChannel_SendException_NUM 9
+#define PyChannel_SendException_RETURN int
+#define PyChannel_SendException_PROTO ( PyChannel*, PyObject*, PyObject* )
+
+#define PyChannel_GetQueue_NUM 10
+#define PyChannel_GetQueue_RETURN PyObject*
+#define PyChannel_GetQueue_PROTO ( PyChannel* )
+
+#define PyChannel_SetPreference_NUM 11
+#define PyChannel_SetPreference_RETURN void
+#define PyChannel_SetPreference_PROTO ( PyChannel*, int )
+
+#define PyChannel_GetBalance_NUM 12
+#define PyChannel_GetBalance_RETURN int
+#define PyChannel_GetBalance_PROTO ( PyChannel* )
+
+// Scheduler Functions
+#define PyScheduler_Schedule_NUM 13
+#define PyScheduler_Schedule_RETURN PyObject*
+#define PyScheduler_Schedule_PROTO ( PyObject*, int )
+
+#define PyScheduler_GetRunCount_NUM 14
+#define PyScheduler_GetRunCount_RETURN int
+#define PyScheduler_GetRunCount_PROTO ( )
+
+#define PyScheduler_GetCurrent_NUM 15
 #define PyScheduler_GetCurrent_RETURN PyObject*
 #define PyScheduler_GetCurrent_PROTO ( )
 
+#define PyScheduler_RunWatchdogEx_NUM 16
+#define PyScheduler_RunWatchdogEx_RETURN PyObject*
+#define PyScheduler_RunWatchdogEx_PROTO ( long,int )
+
 /* Total number of C API pointers */
-#define PyScheduler_API_pointers 1
+#define PyScheduler_API_pointers 17
 
 
 #ifdef SCHEDULER_MODULE
 	/* This section is used when compiling schedulermodule.c */
 
-	class PyTasklet;
+
+    // Tasklet Functions
+	static PyTasklet_Check_RETURN PyTasklet_Check PyTasklet_Check_PROTO;
+
+	static PyTasklet_Setup_RETURN PyTasklet_Setup PyTasklet_Setup_PROTO;
+
+    static PyTasklet_Insert_RETURN PyTasklet_Insert PyTasklet_Insert_PROTO;
+
+    static PyTasklet_GetBlockTrap_RETURN PyTasklet_GetBlockTrap PyTasklet_GetBlockTrap_PROTO;
+
+    static PyTasklet_SetBlockTrap_RETURN PyTasklet_SetBlockTrap PyTasklet_SetBlockTrap_PROTO;
+
+    static PyTasklet_IsMain_RETURN PyTasklet_IsMain PyTasklet_IsMain_PROTO;
+
+    // Channel Functions
+	static PyChannel_New_RETURN PyChannel_New PyChannel_New_PROTO;
+
+    static PyChannel_Send_RETURN PyChannel_Send PyChannel_Send_PROTO;
+
+    static PyChannel_Receive_RETURN PyChannel_Receive PyChannel_Receive_PROTO;
+
+    static PyChannel_SendException_RETURN PyChannel_SendException PyChannel_SendException_PROTO;
+
+    static PyChannel_GetQueue_RETURN PyChannel_GetQueue PyChannel_GetQueue_PROTO;
+
+    static PyChannel_SetPreference_RETURN PyChannel_SetPreference PyChannel_SetPreference_PROTO;
+
+    // Scheduler Functions
+    static PyScheduler_Schedule_RETURN PyScheduler_Shedule PyScheduler_Schedule_PROTO;
+
+    static PyScheduler_GetRunCount_RETURN PyScheduler_GetRunCount PyScheduler_GetRunCount_PROTO;
 
 	static PyScheduler_GetCurrent_RETURN PyScheduler_GetCurrent PyScheduler_GetCurrent_PROTO;
 
+    static PyScheduler_RunWatchdogEx_RETURN PyScheduler_RunWatchdogEx PyScheduler_RunWatchdogEx_PROTO;
+
+
 #else
-	class PyTasklet;
 
 /* This section is used in modules that use schedulermodule's API */
 
 static void** PyScheduler_API;
 
+// Tasklet Functions
+#define PyTasklet_Check \
+	( *(PyTasklet_Check_RETURN( * ) PyTasklet_Check_PROTO)PyScheduler_API[PyTasklet_Check_NUM] )
+
+#define PyTasklet_Setup \
+	( *(PyTasklet_Setup_RETURN( * ) PyTasklet_Setup_PROTO)PyScheduler_API[PyTasklet_Setup_NUM] )
+
+#define PyTasklet_Insert \
+	( *(PyTasklet_Insert_RETURN( * ) PyTasklet_Insert_PROTO)PyScheduler_API[PyTasklet_Insert_NUM] )
+
+#define PyTasklet_GetBlockTrap \
+	( *(PyTasklet_GetBlockTrap_RETURN( * ) PyTasklet_GetBlockTrap_PROTO)PyScheduler_API[PyTasklet_GetBlockTrap_NUM] )
+
+#define PyTasklet_SetBlockTrap \
+	( *(PyTasklet_SetBlockTrap_RETURN( * ) PyTasklet_SetBlockTrap_PROTO)PyScheduler_API[PyTasklet_SetBlockTrap_NUM] )
+
+#define PyTasklet_IsMain \
+	( *(PyTasklet_IsMain_RETURN( * ) PyTasklet_IsMain_PROTO)PyScheduler_API[PyTasklet_IsMain_NUM] )
+
+// Channel Functions
+#define PyChannel_New \
+	( *(PyChannel_New_RETURN( * ) PyChannel_New_PROTO)PyScheduler_API[PyChannel_New_NUM] )
+
+#define PyChannel_Send \
+	( *(PyChannel_Send_RETURN( * ) PyChannel_Send_PROTO)PyScheduler_API[PyChannel_Send_NUM] )
+
+#define PyChannel_Receive \
+	( *(PyChannel_Receive_RETURN( * ) PyChannel_Receive_PROTO)PyScheduler_API[PyChannel_Receive_NUM] )
+
+#define PyChannel_SendException \
+	( *(PyChannel_SendException_RETURN( * ) PyChannel_SendException_PROTO)PyScheduler_API[PyChannel_SendException_NUM] )
+
+#define PyChannel_GetQueue \
+	( *(PyChannel_GetQueue_RETURN( * ) PyChannel_GetQueue_PROTO)PyScheduler_API[PyChannel_GetQueue_NUM] )
+
+#define PyChannel_SetPreference \
+	( *(PyChannel_SetPreference_RETURN( * ) PyChannel_SetPreference_PROTO)PyScheduler_API[PyChannel_SetPreference_NUM] )
+
+// Scheduler Functions
+#define PyScheduler_Schedule \
+	( *(PyScheduler_Schedule_RETURN( * ) PyScheduler_Schedule_PROTO)PyScheduler_API[PyScheduler_Schedule_NUM] )
+
+#define PyScheduler_GetRunCount \
+	( *(PyScheduler_GetRunCount_RETURN( * ) PyScheduler_GetRunCount_PROTO)PyScheduler_API[PyScheduler_GetRunCount_NUM] )
+
 #define PyScheduler_GetCurrent \
 	( *(PyScheduler_GetCurrent_RETURN( * ) PyScheduler_GetCurrent_PROTO)PyScheduler_API[PyScheduler_GetCurrent_NUM] )
+
+#define PyScheduler_RunWatchdogEx \
+	( *(PyScheduler_RunWatchdogEx_RETURN( * ) PyScheduler_RunWatchdogEx_PROTO)PyScheduler_API[PyScheduler_RunWatchdogEx_NUM] )
+
 
 /* Return -1 on error, 0 on success.
  * PyCapsule_Import will set an exception if there's an error.
