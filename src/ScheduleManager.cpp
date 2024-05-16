e6d64315b3edda9dc3e5604776dfa552768844b5
Joseph Frangoudes <josephf@ccpgames.com>
2024-05-16 11:43:13 +0000

    handle error code from PyGreenlet_SetParent

--- src/ScheduleManager.cpp
+++ src/ScheduleManager.cpp
@@ -359,7 +359,10 @@ PyObject* ScheduleManager::run( Tasklet* start_tasklet /* = nullptr */ )
 			currentTaskletParentBlocked = current_tasklet->get_tasklet_parent()->is_blocked();
         }
 
-		current_tasklet->set_parent( ScheduleManager::get_current_tasklet() );
+        if (current_tasklet->set_parent(ScheduleManager::get_current_tasklet()) == -1)
+        {
+			return nullptr;
+        }
 		
         // If set to true then tasklet will be decreffed at the end of the loop
         bool cleanup_current_tasklet = false;
--- src/Tasklet.cpp
+++ src/Tasklet.cpp
@@ -879,10 +879,10 @@ Tasklet* Tasklet::get_tasklet_parent()
 	return m_tasklet_parent;
 }
 
-void Tasklet::set_parent( Tasklet* parent )
+int Tasklet::set_parent( Tasklet* parent )
 {
 	m_tasklet_parent = parent;
-	PyGreenlet_SetParent( this->m_greenlet, parent->m_greenlet );
+	return PyGreenlet_SetParent( this->m_greenlet, parent->m_greenlet );
 }
 
 void Tasklet::clear_parent()
