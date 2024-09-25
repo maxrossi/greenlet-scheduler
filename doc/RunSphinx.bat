f22defd221e1228ebf8f3d8f5a345eb3b8d1d2a1
Thorsteinn Mar Thorsteinsson <thorsteinnm@ccpgames.com>
2024-09-25 16:41:26 +0000

    Generate docs with pythonInterpreter instead of pip + vanilla Python

--- doc/RunSphinx.bat
+++ doc/RunSphinx.bat
@@ -4,9 +4,9 @@
 ::path to sphinx source folder
 ::path to sphinx build folder
 
-python -m venv .venv
-call .\.venv\Scripts\Activate.bat
-pip install sphinx
-pip install sphinx_rtd_theme
-pip install breathe
-python -m sphinx build -E -b html -Dbreathe_projects.doxygen=%1 -c %2 %3 %4 
+::Pre-requirements:
+::Make sure to set the following CMake option (else it won't generate documentation):
+::-DBUILD_DOCUMENTATION=ON
+
+:: Build it using "our pythonInterpreter":
+"%CCP_EVE_PERFORCE_BRANCH_PATH%\eve\client\pythonInterpreter.bat" -m sphinx build -E -b html -Dbreathe_projects.doxygen=%1 -c %2 %3 %4
